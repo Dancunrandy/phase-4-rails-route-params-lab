f1381940abd35d2639090779d3636a3547b5bbb3
dancunrandy <dancunrandy@gmail.com>
2023-06-14 12:02:42 +0300

    commit tree 3de212c96e58380f576e23f7e4085f8f828c59ba

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,21 @@
 class StudentsController < ApplicationController
-
   def index
-    students = Student.all
-    render json: students
+    @students = Student.all
+    render json: @students
   end
 
+  def grades
+    @grades = Student.pluck(:grade).uniq
+    render json: @grades
+  end
+
+  def highest_grade
+    @highest_grade = Student.maximum(:grade)
+    render json: @highest_grade
+  end
+
+  def show
+    @student = Student.find(params[:id])
+    render json: @student
+  end
 end
